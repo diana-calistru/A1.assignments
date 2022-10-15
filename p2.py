05985eebe7d4a983fba949732f4efe264932e2a4
diana-calistru <115427584+diana-calistru@users.noreply.github.com>
2022-10-15 16:16:42 +0300

    Update p2.py

--- p2.py
+++ p2.py
@@ -5,20 +5,34 @@ Determine whether two given natural numbers have property `P`.
 
 import math
 
+
+def digits(n, list, nr):
+    while n != 0:
+        list.append(n % 10)
+        nr = nr + 1
+        n = math.trunc(n / 10)
+
 n1 = int(input("Enter the first number:"))
 n2 = int(input("Enter the second number:"))
 list1 = []
 list2 = []
 nr1 = 0
 nr2 = 0
-
-def digits(n, list, nr):
-    while n != 0:
-        if n%10 in list = False:
-            list.append(n % 10)
-            nr = nr + 1
-        n = math.trunc(n / 10)
+ok=1
 
 digits(n1, list1, nr1)
+digits(n2, list2, nr2)
 
-print(list1[:])
+if nr1>nr2:
+    for i in list1:
+        if i not  in list2:
+            ok=0
+else:
+    for i in list2:
+        if i not  in list1:
+            ok=0
+
+if ok == 1:
+    print("Numbers have the 'P' property.")
+else:
+    print("Numbers don't have the 'P' property.")
